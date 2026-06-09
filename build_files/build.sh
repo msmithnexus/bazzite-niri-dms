a3c286878767fab6b8a26e9cfc50225615dca2e6
irunatbullets <murray@ik.me>
2026-06-09 14:54:41 +0200

    possible fix for #4

--- build_files/build.sh
+++ build_files/build.sh
@@ -52,7 +52,14 @@ systemctl enable greetd
 
 mkdir -p /var/cache/dms-greeter
 
-tee /usr/lib/sysusers.d/greeter.conf <<'EOF'
+mkdir -p /usr/lib/systemd/system/dms.service.d
+
+tee /usr/lib/systemd/system/dms.service.d/selinux.conf > /dev/null <<'EOF'
+[Service]
+SELinuxContext=system_u:system_r:greetd_t:s0
+EOF
+
+tee /usr/lib/sysusers.d/greeter.conf > /dev/null <<'EOF'
 g greeter 767
 u greeter 767 "greetd greeter" /var/lib/greeter
 EOF
